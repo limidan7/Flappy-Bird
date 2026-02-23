7e6a21a0ae698aa7cf5efee603e3efa1c72a8634
Limidan <limidan7@gmail.com>
2026-02-23 19:48:21 +0000

    First Commit

---
+++ Player/character_body_2d.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var gravity: float = 1750
+@export var jump_force: float = -500
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position.x = 35
+	rotation_degrees = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	position.x = 35
+	
+	
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = jump_force
+		
+		
+		
+	move_and_slide()
