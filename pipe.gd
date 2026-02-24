6e9432cc8b2887b3d0e4ebca71808c76a91627f2
Limidan <limidan7@gmail.com>
2026-02-24 20:45:03 +0000

    Pipe sprite and random height
    Added pipe sprites and started the code for the moving pipes

--- pipe.gd
+++ pipe.gd
@@ -1,10 +1,11 @@
 extends StaticBody2D
 
-
+const movespeed = -25
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.x = 271
-	position.y = -230
+	position.y = randf_range(0, 160)
+	position.x = 800
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
